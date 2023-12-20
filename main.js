f1fe0b549c3f9e7eb364b9f0ce7c21943bcf1a86
Dennis Alvarado <DennisAlvarado2015@gmail.com>
2023-12-20 10:10:39 -0600

    completado

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-const btnCreateTask = document.querySelector("[data-btn-task]");
-const createTask = (event) => {
-    event.preventDefault;
-    const inputTarea = document.querySelector("[data-input-task]");
-    const value = inputTarea.value;
-    inputTarea.value = "";
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("div");
-    list.appendChild(task);
-    const content = ` 
+let idCounter = 0;
+const input =document.querySelector('input[type="text"]');
+
+
+
+userInput.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    addTask();
+
+});
+
+
+let addTask = ()=>{
+    idCounter++
+    let newValue = input.value
+    list.innerHTML +=`
+       <div class="task-container" id="${idCounter}">
             <label for="">
                 <input type="checkbox">
-                ${value}
+               ${newValue}
             </label>
-            <img src="./images/bin.png" class="closeBtn">
-            `
-    task.innerHTML = content;
-    task.classList.add("task-container")    
+            <img src="./images/bin.png" alt="" class="closeBtn">
+        </div>
+        `
+    input.value = "";
+    updateStats();
 }
 
-btnCreateTask.addEventListener("click", createTask);
+list.addEventListener('click', (event)=>{
+    if(event.srcElement.nodeName == 'INPUT'){
+        updateStats();
+    }else if(event.srcElement.nodeName == 'IMG'){
+        deleteTask(event.srcElement.parentNode.id);      
+    }
+});
 
+let updateStats = () =>{
+    let element = list.querySelectorAll('div');
+    let checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
+    stats.innerHTML = `<p>Tareas pendientes ${element.length} Completadas ${checkbox.length}</p>`
+};
 
-
-
-
-
+let deleteTask = (id)=>{
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updateStats();
+};
